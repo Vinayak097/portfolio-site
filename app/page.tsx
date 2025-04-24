edd2c8920e92abf3e5dfa78d51b8257bb804f116
vinayfishyhub <vinay.fishyhub@gmail.com>
2025-04-24 14:09:15 +0530

    build error

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Header from './components/ui/Header';
 import Footer from './components/ui/Footer';
 import ScrollToTop from './components/ui/ScrollToTop';
 import Hero from './components/sections/Hero';
-import About from './components/sections/About';
+
 import Skills from './components/sections/Skills';
 import Projects from './components/sections/Projects';
 
